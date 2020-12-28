e9c935fae506379645413f3e6274e7b392de7c31
dreagonm <1748281467@qq.com>
2020-12-28 11:20:51 +0800

    [Update] Designed Frontend Methods

--- frontend/frontend.h
+++ frontend/frontend.h
@@ -4,5 +4,18 @@
 
 #ifndef PLANEMANAGESYSTEM_FRONTEND_H
 #define PLANEMANAGESYSTEM_FRONTEND_H
-
+#include "../backend/backend.h"
+class Worker{
+    void InitWork();
+    void AddAirlineWork();
+    void EraseAirlineWork();
+    void AddSeatWork();
+    void EraseSeatWork();
+    void ViewAirlineWork();
+    void AdminWork();
+    void OrderWork();
+    void CancelWork();
+    void ViewWork();
+    void UserWork();
+};
 #endif //PLANEMANAGESYSTEM_FRONTEND_H
