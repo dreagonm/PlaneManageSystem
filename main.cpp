a01c7de1b87ef69aa04d43d105d7ff6f49684a56
dreagonm <1748281467@qq.com>
2020-12-29 09:31:55 +0800

    [Update] Prepared For Test Framework

--- main.cpp
+++ main.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 #include "frontend/frontend.h"
+#include "global_config.h"
+
+#ifndef DEBUG
+
 Worker Main;
+
 int main() {
     Main.InitWork();
-    while(1){
-        if(!Main.ErrorAndRetry("enter N to Exit"))
-            break;
-        if(Main.Getpermission()==0)
+    bool firstrun = true;
+    while (1) {
+        if (!firstrun)
+            if (!Main.ErrorAndRetry("enter n to Exit,enter y to Continue"))
+                break;
+        if (firstrun)
+            firstrun = false;
+        if (Main.Getpermission() == 0)
             Main.LoginWork();
-        if(Main.Getpermission()==1)
+        if (Main.Getpermission() == 1)
             Main.UserWork();
-        if(Main.Getpermission()==2)
+        if (Main.Getpermission() == 2)
             Main.AdminWork();
     }
     std::cout << "Goodbye!" << std::endl;
     return 0;
 }
+
+#endif
---
+++ tests/test_database.cpp
@@ -0,0 +1,13 @@
+//
+// Created by 17482 on 2020/12/29.
+//
+#include <iostream>
+#include "test_database.h"
+#include "../database/database.h"
+#include "../global_config.h"
+#ifdef DEBUG
+using namespace std;
+int main(){
+    cout<<"hello"<<endl;
+}
+#endif
