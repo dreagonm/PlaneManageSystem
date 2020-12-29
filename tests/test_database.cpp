3d19a9d5f160bba81014e84a5129f6d4da33723d
dreagonm <1748281467@qq.com>
2020-12-29 15:33:02 +0800

    [Test] Record Class Passed Test

--- tests/test_database.cpp
+++ tests/test_database.cpp
@@ -8,6 +8,37 @@
 #ifdef DEBUG
 using namespace std;
 int main(){
-    cout<<"hello"<<endl;
+
+
 }
+
+/*----------------- TEST_RECORD
+    Record TestRecord;
+    TestRecord.AddRecordField("a","AD");
+    TestRecord.AddRecordField("b","BC");
+    TestRecord.AddRecordField("c","DE");
+    auto Data=TestRecord.GetAllFields();
+    for(auto it=Data.begin();it!=Data.end();it++){
+        cout<<(*it).first<<" "<<(*it).second<<endl;
+    }
+    cout<<TestRecord.GetRecordField("a")<<endl;
+    TestRecord.EraseRecordField("a");
+    Data=TestRecord.GetAllFields();
+    for(auto it=Data.begin();it!=Data.end();it++){
+        cout<<(*it).first<<" "<<(*it).second<<endl;
+    }
+    cout<<TestRecord.CheckRecord("b","BC")<<endl;
+    cout<<TestRecord.CheckRecord("b","CD")<<endl;
+    set<string> S;
+    S.insert("a");
+    S.insert("b");
+    S.insert("d");
+    TestRecord.MaintainRecord(S);
+    Data=TestRecord.GetAllFields();
+    for(auto it=Data.begin();it!=Data.end();it++){
+        cout<<(*it).first<<" "<<(*it).second<<endl;
+    }
+    ----------*/
+
 #endif
+
